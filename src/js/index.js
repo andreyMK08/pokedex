a541aa0b5f9d316595b22c44fa62c7ad2a4692c0
andreyMK08 <71525164+andreyMK08@users.noreply.github.com>
2023-05-14 10:23:12 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+console.log(botaoAlterarTema);
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroAtivo) {
+
+        imagemBotaoTrocaTema.setAttribute("src", "./src/imagens/sun.png");
+
+    } else {
+        imagemBotaoTrocaTema.setAttribute("src", "./src/imagens/moon.png");
+
+    };
+
+
+});
