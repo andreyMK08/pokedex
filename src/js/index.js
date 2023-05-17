5f86295966a4f53d2a8f5f9f8f05bad9cba8e837
andreyMK08 <71525164+andreyMK08@users.noreply.github.com>
2023-05-17 18:04:29 -0300

    Encremento-Cartas

--- src/js/index.js
+++ src/js/index.js
@@ -7,21 +7,21 @@ const body = document.querySelector("body");
 const imagemBotaoTrocaTema = document.querySelector(".imagem-botao");
 
 botaoAlterarTema.addEventListener("click", () => {
-
+	
     const modoEscuroAtivo = body.classList.contains("modo-escuro");
-
+	
     body.classList.toggle("modo-escuro");
-
+	
     if (modoEscuroAtivo) {
-
+		
         imagemBotaoTrocaTema.setAttribute("src", "./src/imagens/sun.png");
-
-    } else {
+		
+		} else {
         imagemBotaoTrocaTema.setAttribute("src", "./src/imagens/moon.png");
-
-    };
-
-
+		
+	};
+	
+	
 });
 
 
@@ -30,68 +30,90 @@ var botaoAlterarImagem = document.getElementById('botao-alterar-imagem');
 var imagemBotaoTrocaImagem = botaoAlterarImagem.querySelector('img');
 
 var liData = {
-  bulbasaur: {
-    novaImagem: "./src/imagens/card-bulbasaur.png"
-  },
-  Ivysaur: {
-    novaImagem: "./src/imagens/card-ivysaur.png"
-  },
-  squirtle: {
-    novaImagem: "./src/imagens/nova-imagem-squirtle.gif"
-  }
+	bulbasaur: {
+		novaImagem: "./src/imagens/card-bulbasaur.png"
+	},
+	Ivysaur: {
+		novaImagem: "./src/imagens/card-ivysaur.png"
+	},
+	Venusaur: {
+		novaImagem: "./src/imagens/card-venusaur.png"
+	},
+	Charmander: {
+		novaImagem: "./src/imagens/card-Charmander.png"
+	},
+	Charmeleon: {
+		novaImagem: "./src/imagens/card-charmeleon.png"
+	},
+	Charizard: {
+		novaImagem: "./src/imagens/card-charizard.png"
+	},
+    Squirtle: {
+		novaImagem: "./src/imagens/card-squirtle.png"
+	},
+    Wartortle: {
+		novaImagem: "./src/imagens/card-wartortle.png"
+	},
+    Blastoise: {
+		novaImagem: "./src/imagens/card-blastoise.png"
+	},
+    Caterpie: {
+		novaImagem: "./src/imagens/card-caterpie.png"
+	}
 };
 
 botaoAlterarImagem.addEventListener("click", function() {
-  for (var liId in liData) {
-    var liElement = document.getElementById(liId);
-    var liInfo = liData[liId];
-    var trocarImagem = liElement.classList.contains("trocar-imagem");
-
-    if (trocarImagem) {
-      // Voltar para a imagem original
-      imagemBotaoTrocaImagem.setAttribute("src", "./src/imagens/pokeball.png");
-      liElement.innerHTML = liElement.dataset.conteudoOriginal;
-      liElement.classList.remove('nova-imagem-li');
-    } else {
-      // Substituir pela nova imagem
-      imagemBotaoTrocaImagem.setAttribute("src", "./src/imagens/logo-pokebola-aberta.png");
-
-      if (!liElement.dataset.conteudoOriginal) {
+	for (var liId in liData) {
+		var liElement = document.getElementById(liId);
+		var liInfo = liData[liId];
+		var trocarImagem = liElement.classList.contains("trocar-imagem");
+		
+		if (trocarImagem) {
+			// Voltar para a imagem original
+			imagemBotaoTrocaImagem.setAttribute("src", "./src/imagens/pokeball.png");
+			liElement.innerHTML = liElement.dataset.conteudoOriginal;
+			liElement.classList.remove('nova-imagem-li');
+		} else {
+		// Substituir pela nova imagem
+		imagemBotaoTrocaImagem.setAttribute("src", "./src/imagens/logo-pokebola-aberta.png");
+		
+		if (!liElement.dataset.conteudoOriginal) {
         // Salvar o conteúdo original
         liElement.dataset.conteudoOriginal = liElement.innerHTML;
-      }
-
-      // Limpar o conteúdo atual da <li>
-      liElement.innerHTML = '';
-
-      // Criar a nova imagem
-      var novaImagem = document.createElement('img');
-      novaImagem.src = liInfo.novaImagem;
-      novaImagem.alt = "Nova Imagem";
-      novaImagem.classList.add('nova-imagem');
-
-      // Estilizar a nova imagem
-      novaImagem.style.padding = "0";
-      novaImagem.style.width = "100%";
-      novaImagem.style.height = "100%";
-      novaImagem.style.objectFit = "cover";
-
-      // Anexar a nova imagem à <li>
-      liElement.appendChild(novaImagem);
-
-      // Adicionar classes para estilização
-      liElement.classList.add('nova-imagem-li');
-    }
-
-    // Alternar a classe "trocar-imagem"
-    liElement.classList.toggle("trocar-imagem");
-  }
-});
-
-
-
-
-
-
-
-
+		}
+		
+		// Limpar o conteúdo atual da <li>
+		liElement.innerHTML = '';
+		
+		// Criar a nova imagem
+		var novaImagem = document.createElement('img');
+		novaImagem.src = liInfo.novaImagem;
+		novaImagem.alt = "Nova Imagem";
+		novaImagem.classList.add('nova-imagem');
+		
+		// Estilizar a nova imagem
+		novaImagem.style.padding = "0";
+		novaImagem.style.width = "100%";
+		novaImagem.style.height = "100%";
+		novaImagem.style.objectFit = "cover";
+		
+		// Anexar a nova imagem à <li>
+		liElement.appendChild(novaImagem);
+		
+		// Adicionar classes para estilização
+		liElement.classList.add('nova-imagem-li');
+		}
+		
+		// Alternar a classe "trocar-imagem"
+		liElement.classList.toggle("trocar-imagem");
+		}
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+				
